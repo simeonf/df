17d15da346490c25321501f7912f63f87c3f82a4
Simeon Franklin <sfranklin@twitter.com>
2018-01-27 13:03:35 -0800

    Add query_replace and required sub-app to settings

--- df/df/settings.py
+++ df/df/settings.py
@@ -71,6 +71,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'admin_views',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -88,6 +89,7 @@ INSTALLED_APPS = (
     'content_group',
     'search',
     'oldsite',
+    'query_replace',
 )
 
 MIDDLEWARE_CLASSES = (
