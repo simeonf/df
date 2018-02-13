eb5ee1722913aedfb42ac7fef712b30a6192c394
Simeon Franklin <sfranklin@twitter.com>
2018-02-13 15:40:03 -0800

    Add request context

--- df/df/settings.py
+++ df/df/settings.py
@@ -35,6 +35,7 @@ TEMPLATES = [
         'DIRS': [BASE_DIR + "/templates"],
         'OPTIONS': {
             'context_processors': (
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.media',
