7b16441a6f21658ab8858c127232365f272a48c4
Simeon Franklin <simeonf@gmail.com>
2023-10-08 23:49:37 -0700

    Clear cache on menu save

--- df/menu/admin.py
+++ df/menu/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.cache import cache
 from django.contrib import admin
 from menu.models import MenuItem
 
@@ -8,6 +9,10 @@ class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'display', 'order')
     search_fields = ('title',)
     list_filter = ('display',)
+
+    def save_model(self, request, obj, form, change):
+        cache.clear()
+        super(MenuItemAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(MenuItem, MenuItemAdmin)
